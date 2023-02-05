e03f33e2bfbfda4148937b4950c96d120546ef6d
Ivan Reyes <ivanwilf@hotmail.com>
2023-02-05 12:23:23 +0900

    Added html with two btns

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"status": "OK"})
+		c.HTML(200, "index.html", gin.H{"status": "OK"})
 	})
 	r.Run()
 }
