eda5670e58431efbc67294d5191c4191bf7a1f2a
Ivan Reyes <ivanwilf@hotmail.com>
2023-02-05 12:27:59 +0900

    Added js file to handle on off request from client

---
+++ assets/index.js
@@ -0,0 +1,7 @@
+$('#led-on').click(function(){
+  console.log("ON")
+});
+
+$('#led-off').click(function(){
+    console.log("OFF")
+  });
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	fmt.Println("Hello world!")
 	r := gin.Default()
+	r.Static("/assets", "./assets")
 	r.LoadHTMLGlob("templates/*")
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(200, "index.html", gin.H{"status": "OK"})
