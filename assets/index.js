917ddbafd3c3f0871e84f0ddbd2e3613cce3d960
Ivan Reyes <ivanwilf@hotmail.com>
2023-02-05 12:37:25 +0900

    Added jquery ajax function that makes post request to server with action data

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,23 @@
-$('#led-on').click(function(){
-  console.log("ON")
-});
+function postAction(action) {
+  $.ajax(
+    {
+      type: "POST",
+      async: false,
+      url: "/action",
+      data: {
+        action: action
+      },
+      success: function(result) {
+        console.log(result)
+      },
+      error: function(XMLHttpRequest, textStatus, errorThrown){
+        console.log(errorThrown)
+      },
+      dataType: "json"
+    }
+  );
+}
 
-$('#led-off').click(function(){
-    console.log("OFF")
-  });
+$('#led-on').click(function(){postAction("ON")});
+
+$('#led-off').click(function(){postAction("OFF")});
